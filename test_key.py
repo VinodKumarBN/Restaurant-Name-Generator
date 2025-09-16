bc46149fdfafb4d8d7185c663ec8d587fbcbcaa2
Vinod Kumar B N <vinodvinodkumarbn164@gmail.com>
2025-09-16 17:38:36 +0530

    test_key.py commited

--- test_key.py
+++ test_key.py
@@ -1,6 +1,5 @@
-# test_key.py
 import os
-k = os.getenv("OPENROUTER_API_KEY") or os.getenv("OPENAI_API_KEY")
+k = os.getenv("OPENROUTER_API_KEY") 
 print("Has key?", bool(k))
 if k:
     print("Masked:", k[:4] + "..." + k[-4:])
